59e765d6b3535629d93e7bbdff0e2a0a8f6843c4
Fabian Bormann <fabian.bormann@cardanofoundation.org>
2024-01-11 12:27:40 +0100

    fix: import is case-sensitive therefore fix the filename and refactor the submodules

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
 import { decode } from 'cbor-x';
 import { ProgramFlatCodec } from './src/uplc/codec';
 import { hexToUint8Array, toInt8Array } from './src/uplc/utils';
-import { DeBruijnedProgram, Version } from './src/uplc/term';
-import { DeBruijn } from './src/uplc/DeBruijn';
+import { DeBruijnedProgram, Version, fromDeBruijnTerm } from './src/uplc/term';
 
 export const decodeFlatUplc = (
   deserializeContract: string,
@@ -13,7 +12,7 @@ export const decodeFlatUplc = (
   const decoder = toInt8Array(scriptFlat);
 
   const program = ProgramFlatCodec.decodeFlat(decoder);
-  const namedTerm = DeBruijn.fromDeBruijnTerm(program.term);
+  const namedTerm = fromDeBruijnTerm(program.term);
   const appied = new DeBruijnedProgram(version, namedTerm);
   return appied.pretty();
 };
--- src/uplc/Term.ts
+++ src/uplc/Term.ts
@@ -2,6 +2,46 @@ import { Constant } from './utils';
 import { DecoderState, FlatNatural } from '../flat/flat';
 import { FlatConstant, FlatDefaultFun } from './instances';
 
+export const fromDeBruijnTerm = (term: Term): Term => {
+  let idx = 0;
+  const go = (term: Term, env: Array<string>): Term => {
+    switch (term.type) {
+      case TermType.Var: {
+        const binderName = env[term.name!.index - 1];
+        const name = new NamedDeBruijn(binderName, term.name?.index);
+        return Term.Var(name);
+      }
+      case TermType.LamAbs: {
+        const binderName: string = `i_${idx}`;
+        idx += 1;
+        return Term.LamAbs(binderName, go(term.term!, [binderName, ...env]));
+      }
+      case TermType.Apply: {
+        const { f, arg } = term;
+        return Term.Apply(go(f!, env), go(arg!, env));
+      }
+      case TermType.Force: {
+        return Term.Force(go(term.term!, env));
+      }
+      case TermType.Delay: {
+        return Term.Delay(go(term.term!, env));
+      }
+      case TermType.Const: {
+        return term;
+      }
+      case TermType.Builtin: {
+        return term;
+      }
+      case TermType.Error: {
+        return term;
+      }
+      default:
+        throw new Error('not found term type');
+    }
+  };
+  return go(term, []);
+};
+
 export class NamedDeBruijn {
   name: string;
   index: number;
